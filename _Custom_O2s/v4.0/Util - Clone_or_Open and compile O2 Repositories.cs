82dc3b8c14bf9dd1350e3a7c448f24f3b9450ef8
DinisCruz <dinis.cruz@owasp.org>
2012-03-20 18:24:12 +0000

    Scripts to create O2 Installer

---
+++ _Custom_O2s/v4.0/Util - Clone_or_Open and compile O2 Repositories.cs
@@ -0,0 +1,73 @@
+﻿// This file is part of the OWASP O2 Platform (http://www.owasp.org/index.php/OWASP_O2_Platform) and is released under the Apache 2.0 License (http://www.apache.org/licenses/LICENSE-2.0)
+using System;
+using System.Windows.Forms;
+using O2.Kernel;
+using O2.Kernel.ExtensionMethods; 
+using O2.DotNetWrappers.ExtensionMethods;
+using O2.Views.ASCX.classes.MainGUI;
+using O2.XRules.Database.APIs; 
+//O2File:API_MSBuild.cs
+//O2File:API_NGit.cs
+//O2File:API_NGit_O2Platform.cs
+//O2Tag_DontAddExtraO2Files
+
+namespace O2.XRules.Database.Utils
+{
+	public class Util_Sync_and_Compile_O2_Repositories_Test
+	{
+		public void launch()
+		{
+			Util_Sync_and_Compile_O2_Repositories.Main();
+		}
+	}
+    public class Util_Sync_and_Compile_O2_Repositories
+    { 
+    
+    	public Util_Sync_and_Compile_O2_Repositories()
+    	{
+    		
+    	}
+    	public static void Main()
+    	{
+    		buildGui();
+    	}
+		public static void buildGui()
+		{
+			var topPanel = O2Gui.open<Panel>("Util - Sync and Compile O2 Repositories",1000,600); 
+			//var topPanel = panel.clear().add_Panel();  
+			 
+			topPanel.insert_LogViewer(); 
+			var msBuildGui = topPanel.add_MSBuild_Gui();   
+			
+			var nGit_O2 = new API_NGit_O2Platform();			
+			
+			//for the O2 installer:			
+			nGit_O2.LocalGitRepositories 	  = PublicDI.config.CurrentExecutableDirectory.pathCombine("..").fullPath(); // by default it is one  above the current one
+			
+			Action gitSync = 
+				()=>{
+						nGit_O2	.cloneOrPull("O2.FluentSharp")
+							  	.cloneOrPull("O2.Platform.Projects")
+							   	.cloneOrPull("O2.Platform.Scripts"); 
+						"gitSync completed".info();
+					};
+			
+			Action compileProjects = 
+				()=>{
+						msBuildGui.buildProjects(nGit_O2.LocalGitRepositories);
+					};
+				
+			//msBuildGui.tableList.title("O2 Platform - MSBuild results");   
+			
+			msBuildGui.topPanel.insert_Above(40,"Actions")
+					  .add_Link	 	 ("Git Sync", 		 ()=> gitSync())
+					  .append_Link	 ("Compile Projects", ()=> compileProjects())		  
+					  .append_Link	 ("Open Target Folder in Explorer", ()=> nGit_O2.LocalGitRepositories.startProcess())
+					  .append_Link	 ("Start 'O2 Platform.exe'", ()=> nGit_O2.LocalGitRepositories.pathCombine(@"O2.Platform.Projects\binaries\O2 Platform.exe").startProcess())
+					  .append_Label  ("Target Folder:").autoSize().top(2)
+					  .append_TextBox(nGit_O2.LocalGitRepositories).align_Right()
+					  											   .onTextChange((text)=> nGit_O2.LocalGitRepositories = text);
+
+		}
+	}
+}
