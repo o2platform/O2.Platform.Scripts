fcad774a612b4f3bb2474ed58dca952cd43a3ce6
DinisCruz <dinis.cruz@owasp.org>
2012-05-23 17:25:23 +0100

    adding VisualStudio PoC file

--- Utils/ExtensionMethods/WPF_ExtensionMethods.cs
+++ Utils/ExtensionMethods/WPF_ExtensionMethods.cs
@@ -4,6 +4,13 @@ using System;
 //O2File:ElementHost_ExtensionMethods.cs
 //O2File:WPF_WinFormIntegration_ExtensionMethods.cs
 //O2File:XamlCode_ExtensionMethods.cs
+
+//O2Ref:PresentationCore.dll 
+//O2Ref:PresentationFramework.dll
+//O2Ref:System.Xaml.dll
+//O2Ref:WindowsBase.dll
+
+
 namespace O2.XRules.Database.Utils
 {
     public static class WPF_ExtensionMethods
