0cfcc1a80b221c5cebd7882bd5df3efe7417991e
DinisCruz <dinis.cruz@owasp.org>
2012-05-28 13:00:04 +0100

    added installer for NodeJS.cs (download of node.exe)

---
+++ 3rdParty_Tools/_Installers/NodeJS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using O2.Kernel;
+using O2.Kernel.ExtensionMethods;
+using O2.DotNetWrappers.ExtensionMethods; 
+using O2.XRules.Database.Utils;
+
+//O2File:Tool_API.cs
+  
+namespace O2.XRules.Database.APIs 
+{ 
+	public class Install_NodeJS_Test
+	{
+		public void test()
+		{
+			new NodeJS().start();
+		}
+	}
+	public class NodeJS : Tool_API 
+	{	
+		public NodeJS() : this(true)
+		{
+		}
+		
+		public NodeJS(bool installNow)
+		{
+			ToolName = "NodeJS";
+			Install_Uri = "http://nodejs.org/dist/v0.6.18/node.exe".uri();
+			Install_Dir = ToolsDir.pathCombine(ToolName);
+			Executable = Install_Dir.pathCombine("node.exe");    		
+    		
+    		if (installNow)
+    			install();		
+		}
+		
+		
+		public bool install()
+		{
+			"Installing {0}".info(ToolName);
+			return install_JustDownloadFile_into_TargetDir(); 						
+		}
+		
+		public Process start()
+		{
+			if (install())
+				return Install_Dir.pathCombine("node.exe").startProcess();
+			return null;
+		}		
+	}
+}
