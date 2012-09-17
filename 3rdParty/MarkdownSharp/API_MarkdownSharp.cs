2edb52cffe6fb1e39e4998390db6c9293bd31502
Dinis Cruz <dinis.cruz@owasp.org>
2012-09-17 14:14:10 +0100

    Update MarkdownSharp scripts

---
+++ 3rdParty/MarkdownSharp/API_MarkdownSharp.cs
@@ -0,0 +1,104 @@
+﻿// This file is part of the OWASP O2 Platform (http://www.owasp.org/index.php/OWASP_O2_Platform) and is released under the Apache 2.0 License (http://www.apache.org/licenses/LICENSE-2.0)
+using System;
+using System.Linq;
+using System.Windows.Forms; 
+using System.Collections.Generic; 
+using O2.Kernel; 
+using O2.DotNetWrappers.ExtensionMethods;
+using O2.XRules.Database.Utils;
+using MarkdownSharp;
+//O2Ref:MarkdownSharp.dll
+
+namespace O2.XRules.Database.APIs
+{	
+	public class API_MarkdownSharp
+	{						
+		public List<Action> AfterTransform 			{ get; set; } 		
+		public string 		Text 					{ get; set; }
+		public string 		LastText_Transformed 	{ get; set; }
+		public WebBrowser 	Browser					{ get; set; }
+		public TextBox 		TextArea				{ get; set; }
+		
+		public API_MarkdownSharp()
+		{
+			AfterTransform  = new List<Action>();
+			Text = "";
+		}
+	}
+	
+	
+
+	public static class API_MarkdownSharp_ExtensionMethods
+	{
+		public static API_MarkdownSharp transform(this API_MarkdownSharp markdown, string text)
+		{
+			markdown.Text = text;
+			return markdown.transform();
+		}
+		public static API_MarkdownSharp transform(this API_MarkdownSharp markdown)
+		{
+			markdown.LastText_Transformed = markdown.Text.markdown_Transform();
+			markdown.AfterTransform.invoke();		
+			return markdown;
+		}
+		public static API_MarkdownSharp showIn_Browser(this API_MarkdownSharp markdown)
+		{
+			if (markdown.Browser.isNull())
+				markdown.Browser ="Markdown Transformation".popupWindow().add_WebBrowser();
+			//var browser = open.webBrowser();			
+			markdown.Browser.set_Text(markdown.LastText_Transformed);
+			return markdown;
+		}
+		
+		public static API_MarkdownSharp showIn_TextArea(this API_MarkdownSharp markdown)
+		{
+			if (markdown.TextArea.isNull())
+				markdown.TextArea ="Markdown Transformation".popupWindow().add_TextArea();
+			//var browser = open.webBrowser();			
+			markdown.TextArea.set_Text(markdown.LastText_Transformed);
+			return markdown;
+		}
+		
+		public static API_MarkdownSharp syncWith_Browser(this API_MarkdownSharp markdown, WebBrowser browser)
+		{
+			markdown.Browser = browser;
+			markdown.AfterTransform.add(()=>markdown.showIn_Browser());
+			return markdown;
+		}
+		
+		public static API_MarkdownSharp syncWith_TextArea(this API_MarkdownSharp markdown, TextBox textBox)
+		{
+			markdown.TextArea = textBox;
+			markdown.AfterTransform.add(()=>markdown.showIn_TextArea());
+			return markdown;
+		}
+		
+		public static string text(this API_MarkdownSharp markdown)
+		{
+			return markdown.Text;
+		}
+		public static API_MarkdownSharp text(this API_MarkdownSharp markdown , string value)
+		{
+			markdown.Text = value;
+			return markdown;
+		}
+		
+		public static string html(this API_MarkdownSharp markdown)
+		{			
+			return markdown.transform().LastText_Transformed;
+		}
+		
+		
+	}
+	public static class API_MarkdownSharp_ExtensionMethods_String
+	{	
+		public static string markdown_Transform(this string stringToTransform)
+		{
+			return new Markdown().Transform(stringToTransform);						
+		}
+		
+		
+		
+	}
+}
+	
---
+++ 3rdParty/MarkdownSharp/MarkdownSharp_Installer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using O2.Kernel;
+using O2.Kernel.ExtensionMethods;
+using O2.DotNetWrappers.ExtensionMethods; 
+using O2.XRules.Database.Utils;
+
+//O2File:API_NuGet.cs
+
+namespace O2.XRules.Database.APIs 
+{	
+	public class MarkdownSharp_Installer
+	{	
+		public MarkdownSharp_Installer()
+		{			
+			var expectedDll = @"NuGet\MarkdownSharp.1.13.0.0\lib\35\MarkdownSharp.dll";
+			var nugetPackage = "MarkdownSharp";
+			if (expectedDll.assembly().isNull())
+			{
+				var nuGet = new API_NuGet();
+				nuGet.install(nugetPackage);
+			}			
+		}
+						
+	}
+}
