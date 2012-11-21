2e27d509e61c15674d05b37643f343e24d84c50c
Dinis Cruz <dinis.cruz@owasp.org>
2012-11-21 12:05:16 +0000

    some refactoring and name changes

---
+++ 3rdParty/ILSpy/API_ILSpy.cs
@@ -0,0 +1,20 @@
+﻿// This file is part of the OWASP O2 Platform (http://www.owasp.org/index.php/OWASP_O2_Platform) and is released under the Apache 2.0 License (http://www.apache.org/licenses/LICENSE-2.0)
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using O2.Kernel;
+using O2.DotNetWrappers.ExtensionMethods;
+//Installer:ILSpy_Installer.cs!ILSpy\ILSpy.exe
+
+//O2Ref:ILSpy\ILSpy.exe
+
+namespace O2.XRules.Database.APIs 
+{
+	public class API_ILSpy
+	{
+		
+	}
+}
--- 3rdParty/ILSpy/ILSpy_Installer.cs
+++ 3rdParty/ILSpy/ILSpy_Installer.cs
@@ -9,22 +9,20 @@ using O2.XRules.Database.Utils;
 
 namespace O2.XRules.Database.APIs
 {
-	public class ILSpy_Test
+	public class ILSpy_Installer_Test
 	{
 		public void test()
 		{
-			new ILSpy().start(); 
+			new ILSpy_Installer().start(); 
 		}
 	}
-	public class ILSpy : Tool_API 
+	public class ILSpy_Installer : Tool_API 
 	{	
-		public ILSpy()
+		public ILSpy_Installer()
 		{
-			config("ILSpy 2.1.0.1603", 
+			config("ILSpy", 
 				   "http://downloads.sourceforge.net/project/sharpdevelop/ILSpy/2.0/ILSpy_Master_2.1.0.1603_RTW_Binaries.zip".uri(),
-				   "ILSpy.exe");
-    		
-    		//http://ignum.dl.sourceforge.net/project/sharpdevelop/ILSpy/2.0/ILSpy_Master_2.1.0.1603_RTW_Binaries.zip
+				   "ILSpy.exe");    		    		
     		installFromZip_Web(); 		
 		}
 		
