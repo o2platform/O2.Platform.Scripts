9a7fd89b712153db42b468de0fae726fc5b5eca0
Dinis Cruz <dinis.cruz@owasp.org>
2013-05-03 14:28:55 +0100

    Small fix to PowerShell script

---
+++ 3rdParty/GnuCitizen/BadAssProxy_Installer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using O2.Kernel;
+using O2.Kernel.ExtensionMethods;
+using O2.DotNetWrappers.ExtensionMethods; 
+using O2.XRules.Database.Utils;
+
+//O2File:Tool_API.cs
+
+namespace O2.XRules.Database.APIs
+{
+	public class Install_Test
+	{
+		public void test()
+		{
+			new BadAssProxy_Installer().start();
+		}
+	}
+	public class BadAssProxy_Installer : Tool_API 
+	{			
+		
+		public BadAssProxy_Installer()
+		{
+			config("BadAssProxy", 
+				   "https://gnucitizen.googlecode.com/files/badassproxy-WINNT-0.0.zip".uri(),
+				   "binary\\bap.exe");
+			installFromZip_Web(); 							
+		}
+						
+		
+		public Process start()
+		{			
+			if (this.isInstalled())
+				return this.Executable.startProcess();
+			return null;
+		}		
+	}
+}
---
+++ 3rdParty/GnuCitizen/Proxify_Installer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using O2.Kernel;
+using O2.Kernel.ExtensionMethods;
+using O2.DotNetWrappers.ExtensionMethods; 
+using O2.XRules.Database.Utils;
+
+//O2File:Tool_API.cs
+
+namespace O2.XRules.Database.APIs
+{
+	public class Install_Test
+	{
+		public void test()
+		{
+			new Proxify_Installer().start();
+		}
+	}
+	public class Proxify_Installer : Tool_API 
+	{			
+		
+		public Proxify_Installer()
+		{
+			config("Proxify", 
+				   "https://gnucitizen.googlecode.com/files/proxify-WINNT-1.2.zip".uri(),
+				   "proxify.exe");
+			installFromZip_Web(); 							
+		}
+						
+		
+		public Process start()
+		{			
+			if (this.isInstalled())
+				return this.Executable.startProcess();
+			return null;
+		}		
+	}
+}
